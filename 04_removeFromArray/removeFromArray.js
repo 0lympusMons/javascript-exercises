84dc6653ecd633a090c6c09b42c87db89655db3e
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2022-07-20 18:18:37 +0800

    Add 04_removeFromArray assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, toRemove) {
+    // filter
 
-};
+    let newArr = arr.filter(x => {
 
-// Do not edit below this line
-module.exports = removeFromArray;
+        let bool = false;
+
+        for(let i = 1; i<arguments.length; i++){
+            if(x !== arguments[i]){
+                bool=true;
+            }else{
+                bool=false;
+                break;
+            }
+        }
+
+        if(bool===true) return x;
+    }
+    );
+
+    return newArr;
+}
+
+removeFromArray([1, 2, 3, 4], 3, 2);
+
+    // Do not edit below this line
+    module.exports = removeFromArray;
