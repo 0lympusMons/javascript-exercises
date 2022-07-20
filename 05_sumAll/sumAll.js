a1d05cbfeab0990f1e59c8be76b8e95e0045836a
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2022-07-20 20:01:42 +0800

    Add 05_sumAll assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function (begin, end) {
+
+    // Error handlers
+    for(let i = 0; i<arguments.length; i++){
+        if(arguments[i] < 0 || !((typeof arguments [i]) === 'number') ){
+            return 'ERROR';
+        }
+    }
+
+    // parses (begin, end) to (smaller, larger)
+    // handles for begin > end & begin < end
+    let larger, smaller;
+
+    larger = begin > end ? begin : end;
+    smaller = begin > end? end : begin;
+
+    // Sum calculator
+    let sum = 0;
+    for (let i = larger; i >= smaller; i--) {
+        sum += i;
+    }
+
+    return sum;
 
 };
+
+console.log(sumAll(123, 1));
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
