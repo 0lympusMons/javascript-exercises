a11e5ffc98661e5f556ec76b80fff0436a585db7
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2022-07-20 20:27:03 +0800

    Add 07_tempConversion assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
+// fahrenheit to celsius
+// (F − 32) × 5/9
+const ftoc = function (F) {
 
+  celcius = (F - 32) * 5 / 9;
+  //toFixed parses Number to string and limits decimals
+  // Use Number() to parse celcius back from String to Number
+  celcius = Number(celcius.toFixed(1));
+
+  return celcius;
 };
 
-const ctof = function() {
+// celsius to fahrenheit
+// (C × 9/5) + 32 
 
+const ctof = function (C) {
+  fahrenheit = (C * 9 / 5) + 32;
+  fahrenheit = Number(fahrenheit.toFixed(1));
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
