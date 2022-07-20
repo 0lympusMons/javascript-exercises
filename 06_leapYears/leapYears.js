47f3e36ef0b11bdb191d9a0849eac0cdb50baec8
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2022-07-20 20:17:05 +0800

    Add 06_leapYears assignment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,33 @@
-const leapYears = function() {
+const leapYears = function (year) {
 
+    let isLeapYear = false;
+
+    //     >leap years:
+    // >year%4===0
+    // >year%100== not leapyear, unless if year %400===0
+
+    if (year % 4 === 0) {
+
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+
+                isLeapYear = true;
+
+            } else {
+
+                isLeapYear = false;
+            }
+        } else if (year % 100 !== 0) {
+
+            isLeapYear = true;
+
+        }
+    }
+
+    return isLeapYear;
 };
+
+console.log(leapYears(1996));
 
 // Do not edit below this line
 module.exports = leapYears;
