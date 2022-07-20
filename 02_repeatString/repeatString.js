91aa45db3e6d4124480c00da57e14ff914cd158f
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2022-07-20 14:46:36 +0800

    Add 02_repeatString.js assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
 
+    if(n<0) return "ERROR";
+
+    let string2 = "";
+
+    for (let index = 0; index < n; index++) {
+        string2 += string;
+    }
+
+    return string2
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
