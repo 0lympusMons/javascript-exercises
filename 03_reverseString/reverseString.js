a6e82da943bae768fa0766a32fbbee3ef9f8adda
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2022-07-20 15:00:09 +0800

    Add 03_reverseString assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let length = string.length;
+    length--; //index ends at 4 for "hello" argument
+    let reversedString = "";
 
+    //starts from last char of string
+    for(let i =length; i>=0; i--){
+        reversedString += string.charAt(i);
+    }
+
+    return reversedString;
 };
+
+// reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
